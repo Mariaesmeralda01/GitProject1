839a1db83d2679acec1fe927d5edc50801dc0c34
Mariaesmeralda01 <140670630+Mariaesmeralda01@users.noreply.github.com>
2023-07-29 10:50:19 -0400

    Update DemoDay2.java

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -5,7 +5,7 @@ public class DemoDay2 {
 	public static void main(String[] args) {
 		System.out.println("Test");
 		
-	}
+}
 	
 	//Add method 
 	public static void add (int a, int b) {
@@ -16,5 +16,11 @@ public class DemoDay2 {
 	//Subtract method 
 	public static void subtract (int a, int b) {
 		System.out.println(a-b);
+}
+	
+	//Add method 
+	public static void add (int a, int b, int c) {
+		System.out.println(a+b+c);
+		
 	}
 }

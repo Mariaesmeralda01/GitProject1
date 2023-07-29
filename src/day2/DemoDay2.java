034fe33a1cb2d02d2d250333f5300230b0640934
Mariaesmeralda01 <140670630+Mariaesmeralda01@users.noreply.github.com>
2023-07-29 10:51:34 -0400

    Update DemoDay2.java

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -16,6 +16,12 @@ public class DemoDay2 {
 	//Subtract method 
 	public static void subtract (int a, int b) {
 		System.out.println(a-b);
+		
+}
+	
+	//Divide method Yasir
+	public static void divide (int a, int b) {
+		System.out.println(a/b);
 }
 	
 	//Add method 

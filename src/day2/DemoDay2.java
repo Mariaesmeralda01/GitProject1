dee8f0c88522a1870a075f24beca3de5d8e5349d
Esesm <Esesm@LAPTOP-JL4SL9QM.home>
2023-07-29 10:34:21 -0400

    Implemented

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -6,4 +6,15 @@ public class DemoDay2 {
 		System.out.println("Test");
 		
 	}
+	
+	//Add method 
+	public static void add (int a, int b) {
+		System.out.println(a+b);
+		
+}
+	
+	//Subtract method 
+	public static void subtract (int a, int b) {
+		System.out.println(a-b);
+	}
 }
